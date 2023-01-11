23d69e35aac139427a82a4d95603e535d8cbb345
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2023-01-12 10:22:47 +1300

    feat: Add splitting keymaps and terminal keymaps

--- lua/carter/remap.lua
+++ lua/carter/remap.lua
@@ -2,16 +2,49 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>sd", vim.cmd.Ex)
 
 -- Copy and paste
-vim.keymap.set("n", "<leader>y", "\"y+")
-vim.keymap.set("n", "<leader>p", "\"p+")
-
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>p", "\"+p")
 vim.keymap.set("n", ">", "gv>")
 vim.keymap.set("n", "<", "gv<")
 
--- Doubles
+ -- Do not yank x
+vim.keymap.set("n", "x", "\"_x")
 
-vim.keymap.set("i", "(", "()<Esc>i")
-vim.keymap.set("i", "'", "''<Esc>i")
-vim.keymap.set("i", "\"", "\"\"<Esc>i")
-vim.keymap.set("i", "{", "{}<Esc>i")
-vim.keymap.set("i", "[", "[]<Esc>i")
+-- Doubles
+--vim.keymap.set("i", "(", "()<Esc>i")
+--vim.keymap.set("i", "'", "''<Esc>i")
+--vim.keymap.set("i", "\"", "\"\"<Esc>i")
+--vim.keymap.set("i", "{", "{}<Esc>i")
+--vim.keymap.set("i", "[", "[]<Esc>i")
+
+--Splitting
+--function move down
+local function splitHorizontal()
+  vim.cmd("split")
+  vim.cmd("exe \"normal \\<C-w>j\"")
+end
+
+local function splitVertical()
+  vim.cmd("vsplit")
+  vim.cmd("exe \"normal \\<C-w>l\"")
+end
+
+vim.keymap.set("n", "<leader>ws", function() splitHorizontal() end)
+vim.keymap.set("n", "<leader>wv", function() splitVertical() end)
+vim.keymap.set("n", "<leader>wh", "<C-w>h")
+vim.keymap.set("n", "<leader>wj", "<C-w>j")
+vim.keymap.set("n", "<leader>wk", "<C-w>k")
+vim.keymap.set("n", "<leader>wl", "<C-w>l")
+
+--Terminal
+local function makeTerminal()
+splitHorizontal()
+vim.cmd("terminal")
+vim.cmd("exe \"normal i\"")
+end
+
+--Esc terminal back to normal mode
+vim.keymap.set("t", "<Esc>","<C-\\><C-n>")
+
+--Open terminal
+vim.keymap.set("n", "<leader>t",function() makeTerminal() end)
