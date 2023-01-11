89ef69e420648154b2a683d3cc5eb31624873b06
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2023-01-12 12:46:37 +1300

    feat: added git push and resizing windows

--- lua/carter/remap.lua
+++ lua/carter/remap.lua
@@ -29,13 +29,33 @@ local function splitVertical()
   vim.cmd("exe \"normal \\<C-w>l\"")
 end
 
+local function makeSmaller()
+  vim.cmd("res -10")
+end
+
+local function makeBigger()
+  vim.cmd("res +10")
+end
+
+local function makeHorizontalSmaller()
+  vim.cmd("vert res -30")
+end
+
+local function makeHorizontalBigger()
+  vim.cmd("vert res +30")
+end
+
 vim.keymap.set("n", "<leader>ws", function() splitHorizontal() end)
 vim.keymap.set("n", "<leader>wv", function() splitVertical() end)
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
 vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
 vim.keymap.set("n", "<leader>wl", "<C-w>l")
-
+vim.keymap.set("n", "<leader>wr", "<C-w>r")
+vim.keymap.set("n", "<leader>w-", function() makeSmaller() end)
+vim.keymap.set("n", "<leader>w+", function() makeBigger() end)
+vim.keymap.set("n", "<leader>w,", function() makeHorizontalSmaller() end)
+vim.keymap.set("n", "<leader>w.", function() makeHorizontalBigger() end)
 --Terminal
 local function makeTerminal()
 splitHorizontal()
