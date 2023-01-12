a7a99db777a4b8219cb8618862ac85325c5d2039
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2023-01-12 14:40:46 +1300

    feat: Added blankline

--- lua/carter/set.lua
+++ lua/carter/set.lua
@@ -3,7 +3,11 @@ vim.opt.relativenumber = true
 
 vim.opt.smartindent = true
 vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+
 vim.opt.incsearch = true
+vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.cmd.DoMatchParen = true
