dfe65170865e584f0bd018a4b410a09bd9a4e8b4
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2023-05-08 14:05:23 +1200

    feat: gitdiff shortcuts gH and gL, gp push

--- after/plugin/fugutive.lua
+++ after/plugin/fugutive.lua
@@ -1,13 +1,24 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+local function diffgetLeft()
+  print("Pulling left")
+  vim.cmd("diffget //2")
+end
 
+local function diffgetRight()
+  print("Pulling right")
+  vim.cmd("diffget //3")
+end
+
+local function gitPush()
+  print("Pushing...")
+  vim.cmd("Git push")
+end
+
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gp",function() gitPush() end)
+vim.keymap.set("n", "<leader>gH",function() diffgetLeft() end)
+vim.keymap.set("n", "<leader>gL",function() diffgetRight() end)
 
 vim.api.nvim_command('augroup FugitiveToggle')
 vim.api.nvim_command('au!')
 vim.api.nvim_command('au Filetype fugitive nnoremap <buffer> <space>gs <C-w>q')
 vim.api.nvim_command('augroup END')
-
-
---augroup FugitiveToggle
---  autocmd!
---  autocmd Filetype fugitive nnoremap <buffer> <space>g <C-w>q
---augroup END
