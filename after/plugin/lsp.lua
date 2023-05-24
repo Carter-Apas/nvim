cd27624f76fcc02072e92d5f4588bf644eee2e1a
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2023-05-25 06:50:00 +1200

    fix: Allow cmp <enter> to select auto completes, update lsp-zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,3 +2,15 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 lsp.setup()
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }
+})
