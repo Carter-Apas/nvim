5352d2d687e00593348e3d26fa5a6a3e97326239
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2023-05-28 20:34:53 +1200

    fix: Disable TreeSitter folding add disable for > 10000 lines

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "javascript", "typescript", "c", "lua", "rust", "html", "css" },
+  ensure_installed = { "javascript", "typescript", "lua", "html", "css" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -12,6 +12,9 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
+    disable = function(lang, bufnr) -- Disable in large C++ buffers
+            return vim.api.nvim_buf_line_count(bufnr) > 10000
+        end,
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
@@ -26,9 +29,9 @@ require'nvim-treesitter.configs'.setup {
     enable = true
   }
 }
-local vim = vim
-local opt = vim.opt
-opt.foldlevel= 20
-opt.foldmethod= "expr"
-opt.foldexpr= "nvim_treesitter#foldexpr()"
-vim.keymap.set("n", "<leader>z", "za")
+--local vim = vim
+--local opt = vim.opt
+--opt.foldlevel= 20
+--opt.foldmethod= "expr"
+--opt.foldexpr= "nvim_treesitter#foldexpr()"
+--vim.keymap.set("n", "<leader>z", "za")
